3cd253968dec8b851b32e420772d94699c11def2
Adam Riggs <adam@adamriggs.com>
2014-08-11 00:13:19 -0400

    RedditJSON app is still buggy

--- RedditJSON/RedditJSON/AppDelegate.swift
+++ RedditJSON/RedditJSON/AppDelegate.swift
@@ -12,7 +12,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                             
     @IBOutlet var window: NSWindow
     @IBOutlet var subreddit: NSTextField
-    @IBOutlet var jsonTable: NSClipView
+    @IBOutlet var jsonTable: NSTableView
+    
+    var tableData = []
     
     
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
@@ -23,12 +25,31 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
     
-    func getRedditJSON(sub:String){
-        
+    func getRedditJSON(reqString:String){
+        let mySession = NSURLSession.sharedSession()
+        let url:NSURL = NSURL(string:reqString)
+        let networkTask = mySession.dataTaskWithURL(url, completionHandler : {data, response, error -> Void in
+            var err:NSError?
+            var theJSON = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSMutableDictionary
+            let results : NSArray = theJSON["data"]!["children"] as NSArray
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tableData = results
+                self.jsonTable!.reloadData()
+            })
+        })
+        networkTask.resume()
     }
 
     @IBAction func getJSON(sender: AnyObject) {
+        var req = "http://reddit.com/"
         
+        if(subreddit.stringValue != ""){
+            req += "r/\(subreddit.stringValue)"
+        }
+        
+        req += ".json"
+        
+        getRedditJSON(req)
     }
 
 }
